f2d22e345447a80e4d072453e71d2811bd51fc65
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-11-15 14:33:45 +0100

    checking the empty input

---
+++ oefening1/index.js
@@ -0,0 +1,43 @@
+//importing files
+import rl from "readline-sync";
+
+//variables
+const voorNaam = rl.question("Voornaam? ");
+const capVoornaam = voorNaam.substring(1, 0).toUpperCase().concat(".");
+console.log(voorNaam);
+const familieNaam = rl.question("Familienaam? ");
+const capFamilienaam =
+  familieNaam.charAt(0).toUpperCase() + familieNaam.slice(1);
+console.log(familieNaam);
+const adres = rl.question("Straat en huisnummer? ");
+console.log(adres);
+const postcode = rl.question("Postcode? ");
+console.log(postcode);
+const gemeente = rl.question("Gemeente ");
+console.log(gemeente);
+const upperCaseGemeente = gemeente.toUpperCase();
+
+let message = "";
+
+if (voorNaam === "") {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+if (familieNaam === "") {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+if (adres === "") {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+if (postcode === "") {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+if (postcode < 1000 || postcode > 9900) {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+if (gemeente === "") {
+  message += "Minstens één van de ingevulde velden is niet geldig!";
+}
+//ouput
+console.log(capVoornaam + " " + capFamilienaam);
+console.log(postcode + " " + upperCaseGemeente);
+console.log(message);
